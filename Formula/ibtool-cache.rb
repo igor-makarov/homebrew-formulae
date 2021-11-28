a82f748afd28a4294c42d5c6ea982f8b18818e02
Igor Makarov <igormaka@gmail.com>
2021-11-28 12:51:56 +0200

    update formula

--- Formula/ibtool-cache.rb
+++ Formula/ibtool-cache.rb
@@ -5,8 +5,8 @@ class IbtoolCache < Formula
   desc 'A custom build rule script to use a content-addressed cache for `ibtool` (similar to `ccache`)'
   homepage 'https://github.com/igor-makarov/ibtool-cache'
   license 'MIT'
-  sha256 'fd4498bdee078881f4742705448e068340ad7fc4ef2e7d756217e8ade25a764a'
-  head 'https://raw.githubusercontent.com/igor-makarov/ibtool-cache/main/ibtool-cache'
+  sha256 'c465bc29fa2e4e03dfbf14f7fd6f4a80367dc4a79759be98d05f3c549d3e4f90'
+  url 'https://github.com/igor-makarov/ibtool-cache/archive/refs/tags/0.0.1.tar.gz'
 
   def install
     bin.install 'ibtool-cache'
