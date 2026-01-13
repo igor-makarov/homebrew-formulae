d72fbefcdcfeaad015c969d45f672d504180a3f9
Igor Makarov <igormaka@gmail.com>
2026-01-13 12:14:20 +0200

    ttyd-launch 0.0.4

--- Formula/ttyd-launch.rb
+++ Formula/ttyd-launch.rb
@@ -2,8 +2,8 @@ class TtydLaunch < Formula
   desc "A simple iimatey clone"
   homepage "https://github.com/igor-makarov/ttyd-launch"
   license "MIT"
-  url "https://github.com/igor-makarov/ttyd-launch/archive/refs/tags/0.0.3.tar.gz"
-  sha256 "93c221afb5a17da43d87597e7854054d48b15baf67a7f15fa511de87ca97d053"
+  url "https://github.com/igor-makarov/ttyd-launch/archive/refs/tags/0.0.4.tar.gz"
+  sha256 "1c101bd979378cedab436133f7f2543200296ddc1ff661de5e13d685f91f5f0e"
 
   def install
     bin.install "ttyd-launch"
