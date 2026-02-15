4093d8b9ec70fb00cf2b4018daacda15d019efc5
Igor Makarov <igormaka@gmail.com>
2026-02-15 09:37:49 +0200

    Add llenv formula

---
+++ Formula/llenv.rb
@@ -0,0 +1,13 @@
+class Llenv < Formula
+  desc "Lightweight CLI to apply environment variables from JSON configs"
+  homepage "https://github.com/igor-makarov/llenv"
+  license "MIT"
+  url "https://github.com/igor-makarov/llenv/archive/refs/tags/0.0.1.tar.gz"
+  sha256 "9b92accfde2bfa555f7ae8c21e53749c3166900af4c55e43222526104aee4f46"
+
+  depends_on "node"
+
+  def install
+    bin.install "llenv"
+  end
+end
