5120ef045167ba2537e19e32e6b9ad977a939146
Igor Makarov <igormaka@gmail.com>
2026-02-15 10:03:54 +0200

    llenv 0.0.2 — env var interpolation support

--- Formula/llenv.rb
+++ Formula/llenv.rb
@@ -2,8 +2,8 @@ class Llenv < Formula
   desc "Lightweight CLI to apply environment variables from JSON configs"
   homepage "https://github.com/igor-makarov/llenv"
   license "MIT"
-  url "https://github.com/igor-makarov/llenv/archive/refs/tags/0.0.1.tar.gz"
-  sha256 "9b92accfde2bfa555f7ae8c21e53749c3166900af4c55e43222526104aee4f46"
+  url "https://github.com/igor-makarov/llenv/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "4433ead9d8aa84f50e7a3eeaef2c3f3eec76e7b25b23fdd4cce02b678decfcc4"
 
   def install
     bin.install "llenv"
