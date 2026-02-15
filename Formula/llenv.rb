ed5d622cebb0702468b8ea68213c4123818ff5b5
Igor Makarov <igormaka@gmail.com>
2026-02-15 09:39:26 +0200

    Remove node dependency from llenv formula

--- Formula/llenv.rb
+++ Formula/llenv.rb
@@ -5,8 +5,6 @@ class Llenv < Formula
   url "https://github.com/igor-makarov/llenv/archive/refs/tags/0.0.1.tar.gz"
   sha256 "9b92accfde2bfa555f7ae8c21e53749c3166900af4c55e43222526104aee4f46"
 
-  depends_on "node"
-
   def install
     bin.install "llenv"
   end
